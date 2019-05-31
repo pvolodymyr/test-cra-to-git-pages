f174c889d78ce8c39cece480257973d2f72ea5e3
pvolodymyr <pvolodymyr@protonmail.com>
2019-05-31 11:27:16 +0300

    test

---
+++ docs/precache-manifest.2ab8fe3dea3b064bb13b45c2610f2fe9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23135a88d33082fbea6d9508b9eaa91d",
+    "url": "/test-cra-to-git-pages/index.html"
+  },
+  {
+    "revision": "f65d2f4fa8e3651173ed",
+    "url": "/test-cra-to-git-pages/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "c60fa686a07f5b7389c1",
+    "url": "/test-cra-to-git-pages/static/js/2.ca4fedf9.chunk.js"
+  },
+  {
+    "revision": "f65d2f4fa8e3651173ed",
+    "url": "/test-cra-to-git-pages/static/js/main.2abc3b74.chunk.js"
+  },
+  {
+    "revision": "b2c7563e543f907b446d",
+    "url": "/test-cra-to-git-pages/static/js/runtime~main.64f3746f.js"
+  }
+]);
